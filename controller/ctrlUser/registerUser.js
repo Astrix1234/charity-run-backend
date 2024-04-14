b7d487502c9a9ec6645761ea169ecba0820081cf
Asia <dobrowolska.krakow@gmail.com>
2024-04-14 13:14:37 +0200

    register correction

--- controller/ctrlUser/registerUser.js
+++ controller/ctrlUser/registerUser.js
@@ -2,7 +2,8 @@ import userService from "#service/userService.js";
 import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
-  const { email, password, language, name, surname, phone, consent } = req.body;
+  const { email, password, language, name, surname, agreementStatements } =
+    req.body;
   try {
     const existingUser = await userService.findUserByEmail(email);
     if (existingUser) {
